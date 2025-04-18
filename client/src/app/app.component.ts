9afab646108e9fb51c6402d0c0719be843873a63
frunjik <frunjik@gmail.com>
2025-04-18 18:59:03 +0200

    cleanup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component, signal, WritableSignal } from '@angular/core';
-import { FileBrowserComponent } from './file-browser/file-browser/file-browser.component';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
-import { JsonPipe } from '@angular/common';
 
 import { ppt } from '../../../ppt';
-import { PPTField } from '../../../ppt/models';
+import { PPTField } from '../../../ppt/core';
 
 @Component({
     selector: 'app-root',

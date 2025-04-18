5ba5adb56b3ed95e0d0b3e6a2045199fa34cb790
frunjik <frunjik@gmail.com>
2025-04-18 19:30:29 +0200

    form

--- client/src/app/ppt/form/ppt-form/ppt-form.component.ts
+++ client/src/app/ppt/form/ppt-form/ppt-form.component.ts
@@ -1,15 +1,57 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { PPTField, PPTModel } from '../../../../../../ppt/core';
+import { JsonPipe } from '@angular/common';
 
 @Component({
-  selector: 'ppt-form',
-  templateUrl: './ppt-form.component.html',
-  styleUrl: './ppt-form.component.scss',
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'ppt-form',
+    templateUrl: './ppt-form.component.html',
+    styleUrl: './ppt-form.component.scss',
+    imports: [
+        JsonPipe,
+        ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSelectModule],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PPTFormComponent {
 
+    formFields: PPTField[] = [
+        {
+            id: 'a',
+            type: 'string',
+            name: 'a'
+        },
+        {
+            id: 'b',
+            type: 'string',
+            name: 'b'
+        },
+        {
+            id: 'c',
+            type: 'string',
+            name: 'c'
+        }
+    ];
+
+    formDefinition: PPTModel = {
+        id: '',
+        name: 'test',
+        fields: this.formFields
+    };    
+
+    formGroup = this.createFormGroup(this.formDefinition);
+
+    createFormField(field: PPTField) {
+        return new FormControl(field.name)
+    }
+
+    createFormGroup(model: PPTModel) {
+        const fields: any = {};
+        model.fields.forEach((field) => {
+            fields[field.id] = this.createFormField(field);
+        });
+        return new FormGroup(fields);
+    }
 }

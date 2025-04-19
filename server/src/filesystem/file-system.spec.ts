c03e0738c2db01d0ef4a1e0f15dd6155d39cc19d
frunjik <frunjik@gmail.com>
2025-04-19 22:28:01 +0200

    preparing for tests

--- server/src/filesystem/file-system.spec.ts
+++ server/src/filesystem/file-system.spec.ts
@@ -1,8 +1,7 @@
-import {PPTFileSystemService}  from './file-system.service';
+import { PPTFileSystemService } from '../ppt/services/file-system/file-system.service';
 
 describe('', () => {
     it('', () => {
-
         const fs = new PPTFileSystemService();
         expect(fs).toBeDefined();
     });
--- server/src/ppt/services/file-system/file-system.service.ts
+++ server/src/ppt/services/file-system/file-system.service.ts
@@ -2,6 +2,25 @@ import fs from 'fs';
 import path from 'path';
 import { PPTFolderEntry } from './types.js';
 
+export interface PPTFS {
+    readFile(filename: string): Promise<string>;
+    writeFile(filename: string, contents: string): Promise<void>;
+    readFolder(foldername: string): Promise<PPTFolderEntry[]>;
+}
+
+const nodeFS: PPTFS = {
+    readFile: function(filename: string): Promise<string> {
+        return fs.promises.readFile(filename, 'utf-8');
+    },
+    writeFile: function(filename: string, contents: string): Promise<void> {
+        return fs.promises.writeFile(filename, contents);
+    },
+    readFolder: function(foldername: string): Promise<PPTFolderEntry[]> {
+        return readFoldernames(foldername)
+            .then(createFolderEntriesFromFilenames);
+    }
+};
+
 export function readFile(filename: string): Promise<string> {
     return fs.promises.readFile(filename, 'utf-8');
 }
@@ -17,28 +36,41 @@ function createFolderEntry(filename: string, isFolder: boolean): PPTFolderEntry 
     };
 }
 
-async function createFolderEntries(foldername: string): Promise<PPTFolderEntry[]> {
-    const names = await fs.promises.readdir(foldername);
-    const stats = await Promise.all(names.map(e => fs.promises.stat(path.join(foldername, e))));
-    return names.map((name, i) => createFolderEntry(name, stats[i].isDirectory()));
+async function readFoldernames(foldername: string): Promise<string[]> {
+    return fs.promises.readdir(foldername);
 }
 
-export class PPTFileSystemService {
-    constructor(public readonly rootpath: string = '') {}
+async function createFolderEntriesFromFilenames(filenames: string[]): Promise<PPTFolderEntry[]> {
+    return Promise.all(filenames.map((filename) => {
+        return fs.promises.stat(filename).then((stat) => {
+            return createFolderEntry(filename, stat.isDirectory())
+        })
+    }));
+}
 
-    path(folder: string, filename: string): string {
-        return path.join(this.rootpath, folder, filename);
+// async function createFolderEntries(foldername: string): Promise<PPTFolderEntry[]> {
+//     const names = await fs.promises.readdir(foldername);
+//     const stats = await Promise.all(names.map(e => fs.promises.stat(path.join(foldername, e))));
+//     return names.map((name, i) => createFolderEntry(name, stats[i].isDirectory()));
+// }
+
+export class PPTFileSystemService implements PPTFS {
+
+    private _useFS: PPTFS;
+
+    constructor(public readonly rootpath: string = '', useFS: PPTFS = nodeFS) {
+        this._useFS = useFS;
     }
 
     readFile(filename: string): Promise<string> {
-        return readFile(this.path(this.rootpath, filename));
+        return this._useFS.readFile(path.join(this.rootpath, filename));
     }
 
-    writeFile(filename: string, contents: string): Promise<string> {
-        return writeFile(this.path(this.rootpath, filename), contents);
+    writeFile(filename: string, contents: string): Promise<void> {
+        return this._useFS.writeFile(path.join(this.rootpath, filename), contents);
     }
 
     readFolder(foldername: string): Promise<PPTFolderEntry[]> {
-        return createFolderEntries(this.path(this.rootpath, foldername));
+        return this._useFS.readFolder(path.join(this.rootpath, foldername));
     }
 }

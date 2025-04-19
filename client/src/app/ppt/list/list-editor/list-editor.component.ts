3f509a89e03c3cc51df9fa305d65231b07cb7843
frunjik <frunjik@gmail.com>
2025-04-19 06:56:01 +0200

    list edit tinkering

--- client/src/app/ppt/list/list-editor/list-editor.component.ts
+++ client/src/app/ppt/list/list-editor/list-editor.component.ts
@@ -1,10 +1,13 @@
 import { Component, input, output } from '@angular/core';
 import { PPTList } from '@ppt';
 import { PPTListComponent } from "../list/list.component";
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
     selector: 'ppt-list-editor',
-    imports: [PPTListComponent],
+    imports: [
+        MatToolbarModule,
+        PPTListComponent],
     templateUrl: './list-editor.component.html',
     styleUrl: './list-editor.component.scss'
 })
--- client/src/app/ppt/list/list/list.component.ts
+++ client/src/app/ppt/list/list/list.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { PPTList } from '@ppt';
+import { PPTItemComponent } from '../item/item.component';
 
 @Component({
     selector: 'ppt-list',
-    imports: [],
+    imports: [PPTItemComponent],
     templateUrl: './list.component.html',
     styleUrl: './list.component.scss'
 })

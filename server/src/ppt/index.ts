5f1eacfc7d207d2c07b9bf2fbf8cb3ca92a4735d
frunjik <frunjik@gmail.com>
2025-04-19 21:42:43 +0200

    move ppt into server (for now) so we can share it between client and server

--- server/src/ppt/index.ts
+++ server/src/ppt/index.ts
@@ -22,6 +22,8 @@ allModels.forEach((model) => {
 
 
 export * from './core';
-export * from './services/file-system/file-system.service';
+
+// only export what can be shared between client and server
+// export * from './services/file-system/file-system.service';
 
 export const ppt = { version, models };

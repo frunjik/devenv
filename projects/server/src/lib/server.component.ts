07cf335eb519a657548f454f821c0bff1c1df09c
frunjik <frunjik@gmail.com>
2025-04-20 18:23:57 +0200

    copying initial setup

---
+++ projects/server/src/lib/server.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-server',
+  imports: [],
+  template: `
+    <p>
+      server works!
+    </p>
+  `,
+  styles: ``
+})
+export class ServerComponent {
+
+}
---
+++ projects/server/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of server
+ */
+
+export * from './lib/server.service';
+export * from './lib/server.component';

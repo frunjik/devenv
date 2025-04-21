2c81906b3aec85d1861b144d53a2d8438a40cd51
frunjik <frunjik@gmail.com>
2025-04-21 23:25:42 +0200

    test handler

---
+++ projects/server/src/lib/handlers/tests.ts
@@ -0,0 +1,16 @@
+import * as express from 'express';
+// import { FailureResponseBody } from './types.js';
+import { SuccessResponseBody } from './types.js';
+
+export async function runTest(req: express.Request, res: express.Response, next: express.NextFunction) {
+   
+    const testname = req.query['test'] as string;
+
+    const response: SuccessResponseBody<string> = {
+        data: testname
+    };
+
+    res.json(response);
+
+}
+

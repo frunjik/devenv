b70f13c57090ec3b5222101f75506679e41562af
frunjik <frunjik@gmail.com>
2025-04-21 13:52:47 +0200

    fix component name

--- projects/client/src/app/ppt/text/text.component.spec.ts
+++ projects/client/src/app/ppt/text/text.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TextComponent } from './text.component';
+import { PPTTextComponent } from './text.component';
 
 describe('TextComponent', () => {
-    let component: TextComponent;
-    let fixture: ComponentFixture<TextComponent>;
+    let component: PPTTextComponent;
+    let fixture: ComponentFixture<PPTTextComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [TextComponent]
+            imports: [PPTTextComponent]
         })
             .compileComponents();
 
-        fixture = TestBed.createComponent(TextComponent);
+        fixture = TestBed.createComponent(PPTTextComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });

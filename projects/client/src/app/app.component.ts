1a01d879cd1f4b7b5b8c66874718beb7a6784e37
frunjik <frunjik@gmail.com>
2025-04-23 07:34:49 +0200

    cleanup

--- projects/client/src/app/app.component.ts
+++ projects/client/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { MatButton } from '@angular/material/button';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, MatToolbarModule, MatButton, RouterLink],
+    imports: [RouterOutlet, MatToolbarModule, RouterLink],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
@@ -24,18 +24,4 @@ export class AppComponent {
     get host(): string {
         return this._bs.host;
     }
-
-    editFile() {
-    }
-    
-    browseFolder() {
-
-        this._rt.navigate(['/ppt/edit', 'fullpathfilename', './scratch/TODO.md'])
-
-        // this._bs.loadFolderEntries('')
-        //     .subscribe(() => {
-
-        //     });
-    }
-
 }
